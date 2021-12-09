cf75c2e35edf25632fa4b70697db9ca6786c2ca5
EXCALOFRIO <alejandro.ramirezlarena@usp.ceu.es>
2021-12-09 13:59:23 +0100

    V2.3

---
+++ my-app/src/main/java/mtp-bases/org/App.java
@@ -0,0 +1,13 @@
+package mtp-bases.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
